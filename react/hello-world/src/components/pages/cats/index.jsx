219182a739f7fee0ebc7f224871c9cd951d7fbb0
Liem Nguyen <liem1762001@gmail.com>
2023-09-05 09:00:31 +0700

    feat: scrollIntoView

---
+++ react/hello-world/src/components/pages/cats/index.jsx
@@ -0,0 +1,56 @@
+import { useRef, useState } from 'react'
+import { flushSync } from 'react-dom'
+import './Cat.scss'
+
+const Cats = () => {
+  const selectedRef = useRef(null)
+  const [index, setIndex] = useState(0)
+
+  return (
+    <div className="cats">
+      <nav>
+        <button
+          onClick={() => {
+            flushSync(() => {
+              if (index < catList.length - 1) {
+                setIndex(index + 1)
+              } else {
+                setIndex(0)
+              }
+            })
+            selectedRef.current.scrollIntoView({
+              behavior: 'smooth',
+              block: 'nearest',
+              inline: 'center',
+            })
+          }}
+        >
+          Next
+        </button>
+      </nav>
+      <div>
+        <ul className="cats-list">
+          {catList.map((cat, i) => (
+            <li
+              key={cat.id}
+              ref={index === i ? selectedRef : null}
+              className={index === i ? 'active' : ''}
+            >
+              <img src={cat.imageUrl} alt={'Cat #' + cat.id} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const catList = []
+for (let i = 0; i < 10; i++) {
+  catList.push({
+    id: i,
+    imageUrl: 'https://placekitten.com/250/200?image=' + i,
+  })
+}
+
+export default Cats
